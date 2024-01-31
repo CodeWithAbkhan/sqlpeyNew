df7f10e8565d388c89810e48690a56e4bbd67d3f
CodewithAbkhan <awahab.akhan@gmail.com>
2024-01-31 12:51:31 +0500

    search added

---
+++ main.min.9e403a07cf93acc04c92ff004d87dd03d93ea2ac475bbb69443afecce166a976.js
@@ -0,0 +1,13 @@
+(()=>{(function(){const s=document.querySelector(".darkmode-toggle input"),o="light",e="dark";let i=localStorage.theme===e||!("theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches;i?(document.documentElement.classList.add(e),s.checked=!0):(document.documentElement.classList.remove(e),s.checked=!1),s.addEventListener("change",function(){this.checked?(localStorage.theme=e,document.documentElement.classList.add(e)):(localStorage.theme=o,document.documentElement.classList.remove(e))});const a=document.getElementById("navbar-menu-toggle"),t=document.getElementById("navbar-menu"),r=document.getElementById("navbar-lang-toggle"),n=document.getElementById("navbar-lang");document.addEventListener("click",function(e){const s=e.target;a.contains(s)?(n&&n.classList.add("hidden"),t&&t.classList.toggle("hidden")):r.contains(s)?(t&&t.classList.add("hidden"),n&&n.classList.toggle("hidden")):(t&&t.classList.add("hidden"),n&&n.classList.add("hidden"))})})();function t(e,t){const n=document.getElementById("results");if(console.log("main.js Results:",e),e.length){let s="";for(const o in e){const n=t[e[o].ref],i=n.title.split(" ");i.length>1&&(s+=`
+          <li class="flex flex-row items-center gap-y-3 mt-6 mx-2 md:mx-0 rounded-lg shadow-md bg-white dark:bg-gray-700">
+            <div class="flex flex-col w-full  p-6 mt-6 mx-2 md:mx-0">
+              <h2 class="text-3xl font-semibold text-slate-800 dark:text-slate-200">
+                <a href="${n.url}">${n.title}</a>
+              </h2>
+              <div class="flex flex-wrap gap-2 mt-2">
+              <p class="py-1 text-sm mx-auto  text-gray-800  dark:text-white ">
+              ${n.description}
+              </p>
+              </div>
+            </div>
+          </li> `)}n.innerHTML=s}else n.innerHTML="No results found."}var n=new URLSearchParams(window.location.search),e=n.get("query");if(console.log("Query:",e),e){document.getElementById("search-input").setAttribute("value",e);const n=lunr(function(){this.ref("id"),this.field("title",{boost:15}),this.field("description");for(const e in window.store)this.add({id:e,title:window.store[e].title,description:window.store[e].description})});console.log("Index:",n);const s=n.search(e);console.log("Search Results2:",s),t(s,window.store),document.getElementById("search-title").innerText="Search Results for "+e}})()
