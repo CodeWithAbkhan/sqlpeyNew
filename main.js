599ed5a05738c8bd33fd8f0a7e431660fe046e94
wahab-weeinvent <wahab@weeinvent.com>
2024-10-08 15:28:35 +0500

    dotnet blogs

---
+++ main.js
@@ -0,0 +1,97 @@
+(() => {
+  // <stdin>
+  (function() {
+    const themeToggle = document.querySelector(".darkmode-toggle input");
+    const light = "light";
+    const dark = "dark";
+    let isDark = localStorage.theme === dark || !("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (isDark) {
+      document.documentElement.classList.add(dark);
+      themeToggle.checked = true;
+    } else {
+      document.documentElement.classList.remove(dark);
+      themeToggle.checked = false;
+    }
+    themeToggle.addEventListener("change", function() {
+      if (this.checked) {
+        localStorage.theme = dark;
+        document.documentElement.classList.add(dark);
+      } else {
+        localStorage.theme = light;
+        document.documentElement.classList.remove(dark);
+      }
+    });
+    const navbarMenuToggle = document.getElementById("navbar-menu-toggle");
+    const navbarMenu = document.getElementById("navbar-menu");
+    const navbarLangToggle = document.getElementById("navbar-lang-toggle");
+    const navbarLang = document.getElementById("navbar-lang");
+    document.addEventListener("click", function(event) {
+      const target = event.target;
+      if (navbarMenuToggle.contains(target)) {
+        navbarLang && navbarLang.classList.add("hidden");
+        navbarMenu && navbarMenu.classList.toggle("hidden");
+      } else if (navbarLangToggle.contains(target)) {
+        navbarMenu && navbarMenu.classList.add("hidden");
+        navbarLang && navbarLang.classList.toggle("hidden");
+      } else {
+        navbarMenu && navbarMenu.classList.add("hidden");
+        navbarLang && navbarLang.classList.add("hidden");
+      }
+    });
+  })();
+  function displayResults(results, store) {
+    const searchResults = document.getElementById("results");
+    console.log("main.js Results:", results);
+    if (results.length) {
+      let resultList = "";
+      for (const _ in results) {
+        const item = store[results[_].ref];
+        const titleWords = item.title.split(" ");
+        if (titleWords.length > 1) {
+          resultList += `
+          <li class="flex flex-row items-center gap-y-3 mt-6 mx-2 md:mx-0 rounded-lg shadow-md bg-white dark:bg-gray-700">
+            <div class="flex flex-col w-full  p-6 mt-6 mx-2 md:mx-0">
+              <h2 class="text-3xl font-semibold text-slate-800 dark:text-slate-200">
+                <a href="${item.url}">${item.title}</a>
+              </h2>
+              <div class="flex flex-wrap gap-2 mt-2">
+              <p class="py-1 text-sm mx-auto  text-gray-800  dark:text-white ">
+              ${item.description}
+              </p>
+              </div>
+            </div>
+          </li> `;
+        }
+      }
+      searchResults.innerHTML = resultList;
+    } else {
+      searchResults.innerHTML = "No results found.";
+    }
+  }
+  var params = new URLSearchParams(window.location.search);
+  var query = params.get("query");
+  console.log("Query:", query);
+  if (query) {
+    document.getElementById("search-input").setAttribute("value", query);
+    const idx = lunr(function() {
+      this.ref("id");
+      this.field("title", {
+        // boost search to 15
+        boost: 15
+      });
+      this.field("description");
+      for (const key in window.store) {
+        this.add({
+          id: key,
+          title: window.store[key].title,
+          description: window.store[key].description
+        });
+      }
+    });
+    console.log("Index:", idx);
+    const results = idx.search(query);
+    console.log("Search Results2:", results);
+    displayResults(results, window.store);
+    document.getElementById("search-title").innerText = "Search Results for " + query;
+  }
+})();
